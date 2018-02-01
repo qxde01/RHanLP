5d9dc08630dedfba903938ebc5d5bbbe3c46f829
qxde01 <qxde01@gmail.com>
2018-02-01 16:33:16 +0800

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -2,23 +2,3 @@
   x <- substr(x, 2, nchar(x) - 1)
   return(unlist(strsplit(x, ', ')))
 }
-
-.get.java.parameters<-function(){
-  para<-gsub('-Xmx','',getOption("java.parameters"))
-  if(grepl('m',para)){
-    para<-as.integer(gsub('m','',para))
-    if(para<2048){
-      java.parameters<-'-Xmx3g'
-    }
-  }
-  else if(grepl('g',para)){
-    para<-as.integer(gsub('g','',para))
-    if(para<2){
-      java.parameters<-'-Xmx3g'
-    }
-  }
-  else{
-    java.parameters<-getOption("java.parameters")
-  }
-  return(java.parameters)
-}
